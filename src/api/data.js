c5ae12dbd4eac433c6e2701fa8b6ae1eb5b2109e
Laura Weston <l.weston@outlook.com>
2020-12-24 10:56:55 +0000

    updated skills

--- src/api/data.js
+++ src/api/data.js
@@ -19,16 +19,173 @@ export const projects = [
   },
 ];
 
+// https://app.pluralsight.com/profile/data/skillmeasurements/95d6c3e2-aac2-422e-bb55-a069529d7134
 export const skills = [
-  "Angular - Expert",
-  "JavaScript - Expert",
-  "TypeScript - Expert",
-  "C# - Intermediate",
-  "HTML/CSS - Intermediate",
-  "React - Intermediate",
-  "Python - Beginner",
-  "Fortran - Beginner",
-  "SQL - Beginner",
+  {
+    "id": "5ba69940-fc47-4da0-a4e0-85713ee2d730",
+    "title": "JavaScript",
+    "url": "javascript",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/javascript-542e10ea6e.png",
+    "score": 210,
+    "percentile": 85,
+    "level": "expert",
+    "dateCompleted": "2020-10-21T13:51:30.078+00:00",
+    "status": "live"
+  },
+  {
+    "id": "b688041e-b9c5-4b7d-aa47-76a3686b9e11",
+    "title": "TypeScript",
+    "url": "typescript",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/typescript-1169369a3e.png",
+    "score": 159,
+    "percentile": 56,
+    "level": "proficient",
+    "dateCompleted": "2020-10-05T10:26:43.808+00:00",
+    "status": "live"
+  },
+  {
+    "id": "03a98143-dde9-4634-af93-1da684e9d0ea",
+    "title": "C#",
+    "url": "c-sharp",
+    "thumbnailUrl": "https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png",
+    "score": 194,
+    "percentile": 77,
+    "level": "proficient",
+    "dateCompleted": "2020-12-23T13:47:54.919+00:00",
+    "status": "live"
+  },
+  {
+    "id": "4ce2a1fa-1d2a-42ed-95ba-a3d5d7a70bd0",
+    "title": "Python",
+    "url": "python",
+    "thumbnailUrl": "https://pluralsight.imgix.net/paths/python-7be70baaac.png",
+    "score": 154,
+    "percentile": 53,
+    "level": "proficient",
+    "dateCompleted": "2020-01-14T16:30:36.076+00:00",
+    "status": "live"
+  },
+  {
+    "id": "27fc5a80-4d76-4091-838b-394e87585066",
+    "title": "CSS",
+    "url": "css",
+    "thumbnailUrl": "https://pluralsight.imgix.net/paths/path-icons/css-c9b214f0d7.png",
+    "score": 230,
+    "percentile": 91,
+    "level": "expert",
+    "dateCompleted": "2020-12-22T10:27:49.988+00:00",
+    "status": "live"
+  },
+  {
+    "id": "5c2d9f1a-8d26-4ef9-84fe-3c2a14091023",
+    "title": "HTML5",
+    "url": "html5",
+    "thumbnailUrl": "https://pluralsight.imgix.net/paths/path-icons/html5-7ac65c8ec0.png",
+    "score": 247,
+    "percentile": 95,
+    "level": "expert",
+    "dateCompleted": "2020-12-22T09:47:40.271+00:00",
+    "status": "live"
+  },
+  {
+    "id": "b24c51d4-de06-4aee-8713-b92c8d0cfed0",
+    "title": "Angular",
+    "url": "angular",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/angular-14a0f6532f.png",
+    "score": 254,
+    "percentile": 96,
+    "level": "expert",
+    "dateCompleted": "2020-12-03T13:38:08.882+00:00",
+    "status": "live"
+  },
+  {
+    "id": "04973b6c-8ccc-47aa-893d-cb64f32ff494",
+    "title": "React",
+    "url": "react",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/react-0f6558f88f.png",
+    "score": 176,
+    "percentile": 67,
+    "level": "proficient",
+    "dateCompleted": "2020-03-27T13:00:56.651+00:00",
+    "status": "live"
+  },
+  {
+    "id": "f17a386b-1e29-4feb-b6cd-febe8b7ea949",
+    "title": "ASP.NET Core",
+    "url": "aspdotnet-core",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/dot-net-e535ca86d0.png",
+    "score": 134,
+    "percentile": 39,
+    "level": "proficient",
+    "dateCompleted": "2020-09-16T14:05:57.925+00:00",
+    "status": "retake-available"
+  },
+  {
+    "id": "4b89a492-3112-42dd-86e6-ca8d6258c455",
+    "title": "ES6",
+    "url": "es6",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/assessments/images/icons/es6-ZTFHaqZcD2zV.png",
+    "score": 256,
+    "percentile": 97,
+    "level": "expert",
+    "dateCompleted": "2020-12-21T15:23:30.036+00:00",
+    "status": "live"
+  },
+  {
+    "id": "72c00eba-2555-4958-bef3-eab22cf9e1a1",
+    "title": "RxJS",
+    "url": "rxjs",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/assessments/images/icons/rxjs-1TXbfgBFS7wf.png",
+    "score": 197,
+    "percentile": 79,
+    "level": "proficient",
+    "dateCompleted": "2020-12-22T15:15:50.352+00:00",
+    "status": "live"
+  },
+  {
+    "id": "0d453ffd-3bda-4aa6-b0c4-e9ce6c881c4f",
+    "title": "Git",
+    "url": "git",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/macmanagementbasics-17fcd0c81c.png",
+    "score": 201,
+    "percentile": 80,
+    "level": "expert",
+    "dateCompleted": "2020-03-16T12:02:08.812+00:00",
+    "status": "retake-available"
+  },
+  {
+    "id": "976115a0-3897-449e-887d-9d2bbb8192a3",
+    "title": "NPM",
+    "url": "npm",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/assessments/images/icons/npm-EQLkhQpQW4Pa.png",
+    "score": 226,
+    "percentile": 90,
+    "level": "expert",
+    "dateCompleted": "2020-02-18T15:03:39.819+00:00",
+    "status": "retake-available"
+  },
+  {
+    "id": "ca0367aa-bed0-4ea7-a5ee-0c8f8cddddde",
+    "title": "Unity: Fundamentals",
+    "url": "unity-game-development-core-skills",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/paths/images/unity-7499232fd3.png",
+    "score": 140,
+    "percentile": 43,
+    "level": "proficient",
+    "dateCompleted": "2020-12-21T14:54:47.195+00:00",
+    "status": "live"
+  },
+  {
+    "id": "aef2a175-f1a8-45da-bf72-f3e68e3e2625",
+    "title": "Flask",
+    "url": "flask",
+    "thumbnailUrl": "https://pluralsight2.imgix.net/assessments/images/icons/no-logo-jAtNDcyYi00ZWJ.png",
+    "score": 102,
+    "percentile": 21,
+    "level": "proficient",
+    "dateCompleted": "2020-01-15T14:28:33.851+00:00",
+    "status": "live"
+  }
 ];
 
 export const interests = [
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,13 +1,26 @@
 import React from "react";
 import { skills } from "../api/data";
+import pluralsightImg from "../images/PS_logo_F-01.png";
+import pluralsightSkill from "../images/pluralsight.svg";
 
 const Skills = () => {
   return (
     <div id="skills" className="box">
-      <h2>Skills</h2>
+      <h2>
+        <a href="https://app.pluralsight.com/profile/laura-weston" target="_blank" rel="noopener noreferrer">
+          <img src={pluralsightImg} alt="PuralSight" height="45px"/>
+          <img src={pluralsightSkill} alt="Skills"></img>
+        </a>
+      </h2>
       <ul id="skill-list">
         {skills.map((skill) => {
-          return <li key={skill} >{skill}</li>;
+          return (<li key={skill.id} className="skill-item">
+            <div className="skill-row">
+              <img src={skill.thumbnailUrl} alt={skill.url} width="50px" height="50px"></img> 
+              <h3>{skill.title}</h3>
+            </div>
+              <progress className="skill-row" value={skill.score} max="300">skill.score</progress>
+            </li>);
         })}
       </ul>
     </div>
